ccff4fd582e1484b8ff186c78042fde847ae0425
RunyuFang <fangr1@myumanitoba.ca>
2024-08-06 14:29:52 -0500

    alltest

---
+++ app/src/test/java/com/example/assemble/AllUnitTests.java
@@ -0,0 +1,16 @@
+package com.example.assemble;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        NoteManagerTestWithSQLDB.class,
+        NoteManagerTestWithStubDB.class,
+        NoteTest.class,
+        FlashCardManagerTest.class,
+        UserManagerTestWithSQLDB.class,
+        UserManagerTestWithStubDB.class,
+        UserSettingsTest.class,
+})
+public class AllUnitTests {
+}
